de517376ac06e7a2bb88e731b962a55217f104db
EmmyMay <lekeemmy@gmail.com>
2022-07-29 06:19:04 +0100

    included new color

--- src/boot/color.js
+++ src/boot/color.js
@@ -3,5 +3,5 @@ import { setCssVar } from 'quasar'
 export default () => {
   setCssVar('primary', '#343084')
   setCssVar('secondary', '#1f2730')
-  setCssVar('accent', '#665c97')
+  setCssVar('positive', '#e4dfff')
 }
