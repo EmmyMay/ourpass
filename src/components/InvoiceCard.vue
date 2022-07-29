2ed9d1f5b8abebcbccc4c921d1a513bb6034252a
EmmyMay <lekeemmy@gmail.com>
2022-07-29 16:15:23 +0100

    setup props

--- src/components/InvoiceCard.vue
+++ src/components/InvoiceCard.vue
@@ -3,28 +3,28 @@
     <div>
       <h6 class="q-ma-none text-subtitle2 q-mb-md">Invoice Number</h6>
 
-      <p>INV-2022-010</p>
+      <p>{{ invoice?.invoiceNumber }}</p>
 
       <dl class="custom-fit row items-center ">
         <dt class="q-mr-xs text-common">Issued Date:</dt>
-        <dd> <time datetime="2022-01-11"> 11 Jan 2022 </time> </dd>
+        <dd> <time datetime="2022-01-11"> {{ invoice?.dateIssued }} </time> </dd>
       </dl>
       <dl class="custom-fit row items-center">
         <dt class="q-mr-xs text-common">Due Date:</dt>
-        <dd> <time datetime="2022-01-18"> 18 Jan 2022 </time> </dd>
+        <dd> <time datetime="2022-01-18"> {{ invoice?.dueDate }} </time> </dd>
       </dl>
     </div>
     <div class="column q-mt-lg q-mt-sm-none items-sm-end">
       <h6 class="q-ma-none text-subtitle2 q-mb-md">Billed to</h6>
 
-      <p>Zak Grizzly</p>
+      <p>{{ invoice?.billedTo }}</p>
 
       <div class="custom-fit row items-center">
-        <p class="q-mr-xs">Moonlight Agency LTD</p>
+        <p class="q-mr-xs">{{ invoice?.companyName }}</p>
 
       </div>
       <div class="custom-fit row items-center">
-        <p class="q-mr-xs">New York, USA</p>
+        <p class="q-mr-xs">{{ invoice?.location }}</p>
 
       </div>
     </div>
@@ -34,7 +34,11 @@
 
 <script>
 export default {
-
+  props: {
+    invoice: {
+      type: Object,
+    },
+  },
 }
 </script>
 
